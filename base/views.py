1d4e3c21927d522a86be6e501a6569a231bfbe2f
Maybe.Later <stas220044@gmail.com>
2024-01-21 12:14:34 +0200

    Date changes

--- base/views.py
+++ base/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponse
 from django.db.models import Q
+from django.utils.timesince import timesince
+from datetime import datetime, timedelta
 from .models import Room, Topic, Message
 from .forms import RoomForm
 
@@ -68,6 +70,11 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all()
     participants = room.participants.all()
+    for room in room:
+        if datetime.now() - room.created > timedelta(days=7):
+            room.created = room.created.strftime("%B %d, %Y")
+        else:
+            room.created = timesince(room.created)
     if request.method == 'POST':
         message = Message.objects.create(
             user = request.user,
